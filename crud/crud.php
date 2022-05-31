abaa21db1ccdef55c2d70167a23f13a0b7ab5615
Lais.ST <74365785+LaisST@users.noreply.github.com>
2022-05-31 10:44:01 -0300

    Atualização do crud.php
    Inclusão do montar e atualizar

--- crud/crud.php
+++ crud/crud.php
@@ -30,11 +30,94 @@ switch($acao){
         
         break;
     
-    case 'montar':
+        case 'montar':
+        $id = $_GET['id'];
+        $sql = 'SELECT * FROM users WHERE user_id ='. $id;
+        $resultado = mysqli_query($conexao, $sql) or die ("Erro ao retornar dados");
+        
+        //Montando o formulário 
+        echo "<form method='post' name='dados' action='crud.php?acao=atualizar' onSubmit='return enviardados();'>";
+        echo "<table width='588' border='0' align='center' >";
+        
+         while ($registro = mysqli_fetch_array($resultado)){
+            echo "<tr>";
+            echo "<td width='118'><font size'1' face='Verdana, Arial, Helvetica, sans=serif'> Código: </font></td>";
+            echo "<td width='460'>";
+            echo "<input name='id' type='text' class='formbutton' id='id' size='5' maxlenght='10' value=" . $id . " readonly>";
+            echo "</td>";
+            echo "</tr>";
+            
+            echo "<tr>";
+            echo "<td width='118'><font size'1' face='Verdana, Arial, Helvetica, sans=serif'>Nome:</font></td>";
+            echo "<td rowspan='2'><font size='2'>";
+            echo "<style>textarea{resize:none;}</style>";
+            echo "<textarea name='nome' cols='50' rows='1' class='formbutton'>" . htmlspecialchars($registro['user_name']) . "</textarea>";
+            echo "</font></td>";
+            echo "</tr>";
+            
+            echo "<tr>";
+            echo "<tr>";
+            echo "<td width='118'><font size'1' face='Verdana, Arial, Helvetica, sans=serif'> Email: </font></td>";
+            echo "<td width='460'>";
+            echo "<input name='email' type='text' class='formbutton' id='email' size='52' maxlenght='150' value=" . $registro['user_email'] . " ";
+            echo "</td>";
+            echo "</tr>";
+            
+            echo "<tr>";
+            echo "<td width='118'><font size'1' face='Verdana, Arial, Helvetica, sans=serif'>Data:</font></td>";
+            echo "<td width='460'>";
+            echo "<input name='data' type='date' class='formbutton' id='data' size='52' maxlenght='150' value=" . $registro['user_date'] . " ";
+            echo "</td>";
+            echo "</tr>";
+            
+            echo "<tr>";
+            echo "<td width='118'><font size'1' face='Verdana, Arial, Helvetica, sans=serif'>Mensagem:</font></td>";
+            echo "<td rowspan='2'>";
+            echo "<textarea name='tx_mensagem' cols='50' rows='8' class='formbutton'>" . htmlspecialchars($registro['user_mensagem']) . "</textarea>";
+            echo "</font></td>";
+            echo "</tr>";
+            echo "<tr>";
+            
+            echo "<tr>";
+            echo "<td height='22'></td>";
+            echo "<td>";
+            echo "<input name='Submit' type='submit' class='formobjects' value='Atualizar'>";
+            echo "<button type='submit' formaction='crud.php?acao=selecionar'>Selecionar</button>";
+            echo "<input name='Reset' type='reset' class='formobjects' value='Limpar campos'>";
+            echo "</td>";
+            echo "</tr>";
+            echo "</tr>";
+            
+
+        }
+        echo "</table>";
+        echo "</form>";
+        
+
+        
+        mysqli_close($conexao);
         
         break;
         
     case 'atualizar':
+        $codigo = $_POST['id'];
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $data = $_POST['data'];
+        $mensagem = $_POST['tx_mensagem'];
+        
+        $sqlUpdate = "UPDATE users SET user_name = '" . $nome . "', user_email = '" . $email . "', user_date = '" . $data . "', user_mensagem = '" . $mensagem . "' WHERE user_id = '" . $codigo . "'";
+        
+        if (!mysqli_query($conexao, $sqlUpdate)){
+            die('</br> Erro no comando SQL UPDATE: ' . mysqli_error($conexao));
+        }else{
+            echo "<script language='javascript' type='text/javascript'>
+            alert('Dados Atualizados com Sucesso!')
+            window.location.href='crud.php?acao=selecionar'</script>";
+        }
+        
+        mysqli_close($conexao);
+        
         
         break;
         
