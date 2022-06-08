804731c6054c8492735b6169cb643219ff70b68d
Lais.ST <74365785+LaisST@users.noreply.github.com>
2022-06-08 08:09:37 -0300

    inclusão do file init.php

---
+++ MVC (Model-View-Controller)/model/init.php
@@ -0,0 +1,13 @@
+<?php 
+//timezone
+
+    date_default_timezone_set('America/Sao_Paulo');
+
+//conexão com o banco de dados
+
+    define('BD_SERVIDOR', 'localhost');
+    define('BD_USUARIO', 'id18842383_localhost');
+    define('BD_SENHA', '=Wwdm3f&V$Ni8u<r');
+    define('BD_BANCO', 'id18842383_livraria');
+
+?>
