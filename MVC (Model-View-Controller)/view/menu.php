5f72e752d2327106dee4a24291fc96a45b648ec2
Lais.ST <74365785+LaisST@users.noreply.github.com>
2022-06-08 08:13:41 -0300

    Create menu.php

---
+++ MVC (Model-View-Controller)/view/menu.php
@@ -0,0 +1,8 @@
+<hr>
+    <div class="form-group">
+        <div class="mx-auto" style="width: 1200px;">
+            <a type="button" href="cadastro.php" class="btn btn-sucess">Cadastrar</a>
+            <a type="button" href="index.php" class="btn btn-default">Consultar</a>
+        </div>
+    </div>
+</hr>
